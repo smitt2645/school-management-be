74a30a60b3205529f975bc387b27840322ea7910
Your Name <your.email@example.com>
2026-01-01 12:29:06 +0530

    database file updated !

--- Db/database.js
+++ Db/database.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 //   dialect: "mysql",
 // });
 
+const { Sequelize } = require("sequelize");
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -15,7 +17,16 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
+
+module.exports = sequelize;
 
 module.exports = sequelize;
