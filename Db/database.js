1d34efdfba6c58f1eade7ca4a4ff7734b1337ae8
Your Name <your.email@example.com>
2026-01-01 12:46:30 +0530

    commit

--- Db/database.js
+++ Db/database.js
@@ -1,21 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
+const sequelize = new Sequelize("school_inventory", "root", "", {
+  host: "localhost",
+  port: "3308",
+  dialect: "mysql",
+});
 
 module.exports = sequelize;
