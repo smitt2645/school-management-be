efdacb214c9f16859072dd60b1b86ec069c4adbb
Your Name <your.email@example.com>
2026-01-01 13:14:32 +0530

    ignore env file 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const sequelize = require("./Db/database.js");
+require("dotenv").config();
 
 // Routes
 const bookSetRoutes = require("./Routes/index.js");
